fa2873ba020a345ebcf5a004db66cdf8e8471b28
周华健 <944310630@qq.com>
2019-10-21 10:49:57 +0800

    首次

---
+++ de-sign-in/src/main/java/com/win/DeSignInApplication.java
@@ -0,0 +1,13 @@
+package com.win;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeSignInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeSignInApplication.class, args);
+	}
+
+}
